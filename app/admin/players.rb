df16006cc48a2a664a8d42a52a1228ef62fe0e31
Unknown <owaiswizard@gmail.com>
2018-10-11 18:57:41 +0530

    Customize active admin's players resource

--- app/admin/players.rb
+++ app/admin/players.rb
@@ -11,5 +11,47 @@ ActiveAdmin.register Player do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+  # actions :all
 
+  controller do
+    def permitted_params
+      params.permit!
+    end
+  end
+
+  before_create do |player|
+    puts '-'*90
+    # puts player.image
+    puts '-'*90
+    puts '-'*90
+  end
+  
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :position
+    column :nationality
+    column :club
+    column :goals
+    column :age
+    column :height
+    actions
+  end
+
+  filter :name
+  filter :nationality
+  filter :club
+  filter :goals
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :club
+      f.input :goals
+    end
+    f.inputs 'Image' do
+      f.input :image, as: :file
+    end
+    f.actions
+  end
 end
