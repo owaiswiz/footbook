08da4404da81f0e38b334d3936906372c33abd68
Unknown <owaiswizard@gmail.com>
2018-10-11 23:45:23 +0530

    Minor active admin changes

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -112,7 +112,7 @@ ActiveAdmin.setup do |config|
   # roots for each namespace.
   #
   # Default:
-  config.root_to = 'player#index'
+  config.root_to = 'players#index'
 
   # == Admin Comments
   #
